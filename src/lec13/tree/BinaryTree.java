fb13bc0c9f7228b345b324d649c1ec799236cceb
Ketan Mayer-Patel <kmp@cs.unc.edu>
2020-02-26 09:35:40 -0500

    Added setValue and insertValue

--- src/lec13/tree/BinaryTree.java
+++ src/lec13/tree/BinaryTree.java
@@ -3,6 +3,8 @@ package lec13.tree;
 public interface BinaryTree<T> {
 
 	T getValue();
+	void setValue(T value);
+	void insert(T value);
 	BinaryTree<T> getLeft();
 	BinaryTree<T> getRight();
 	void setLeft(BinaryTree<T> left);
